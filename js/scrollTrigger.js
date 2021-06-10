0bd57e399d42a1f26b04a31328a99794d507aaac
Jason Nutt <jnutt367@gmail.com>
2021-06-10 19:28:00 -0400

    commit for last animation

--- js/scrollTrigger.js
+++ js/scrollTrigger.js
@@ -8,7 +8,9 @@ let tl = gsap.timeline({
             pin: false,
             start: "top 75%",
             end: "bottom 45%",
-            toggleActions: "restart none none reverse"
+            toggleActions: "restart none none restart"
         }
     })
-    .fromTo("#man ", { opacity: 0 }, { y: -20, duration: 7, ease:"elastic",opacity: 1 }, 2);
+    .fromTo("#man ", { opacity: 0 }, { y: -20, duration: 7, ease:"elastic",opacity: 1 })
+    .fromTo("#react ", { x: -300,opacity: 0 }, { x: 0, y: -20, duration: 3, ease:"elastic",opacity: 1 },2)
+    .fromTo("#next ", { opacity: 0, y: 100 }, { y: -20, duration: 2, ease:"elastic",opacity: 1 },4);

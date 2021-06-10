e1c1e97d553656c381720010987f6a0b9082bc2d
Jason Nutt <jnutt367@gmail.com>
2021-06-10 13:12:59 -0400

    commit for instructor page

--- js/gsap.js
+++ js/gsap.js
@@ -1,1 +1,3 @@
 gsap.fromTo("h1", { color: "white", y: -200 }, { duration: 3, y: 0, color: "blue" })
+gsap.fromTo("#thinking-cap", { y: -200, x: 0 }, { duration: 3, y: -150, x: -112 })
+gsap.to("#thinking-cap", { stroke: "silver", repeat: 3, strokeWidth: "15", fill: "black" }, 3)

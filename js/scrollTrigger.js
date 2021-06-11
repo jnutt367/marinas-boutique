37270cc0afdd8bc143809034d5a344a8603cfb19
Jason Nutt <jnutt367@gmail.com>
2021-06-11 14:10:51 -0400

    commit for react svg completely modified onto Next svg mods

--- js/scrollTrigger.js
+++ js/scrollTrigger.js
@@ -12,5 +12,5 @@ let tl = gsap.timeline({
         }
     })
     .fromTo("#man ", { opacity: 0 }, { y: -20, duration: 7, ease:"elastic",opacity: 1 })
-    .fromTo("#react ", { x: -300,opacity: 0 }, { x: 0, y: -20, duration: 3, ease:"elastic",opacity: 1 },2)
-    .fromTo("#next ", { opacity: 0, y: 100 }, { y: -20, duration: 2, ease:"elastic",opacity: 1 },4);
+    .fromTo("#react ", { x: -300,opacity: 0 }, { x: 0, y: -20, duration: 3, ease:"elastic",opacity: 1 }, +2)
+    .fromTo("#next ", { opacity: 0, y: 100 }, { y: -20, duration: 2, ease:"elastic",opacity: 1 }, +5);
